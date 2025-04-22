ce97eeebddd2f5feaced6c70c312865205a1c8a1
sm111111 <shekharmali2001@gmail.com>
2025-04-22 12:39:16 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://pdf-converter-g39r.onrender.com',
     credentials: true
 }));
 
