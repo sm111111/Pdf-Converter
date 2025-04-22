5b2ce6368d40f5bbc9df7c6d6b1aa4c2aefd27b8
sm111111 <shekharmali2001@gmail.com>
2025-04-22 12:33:56 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
